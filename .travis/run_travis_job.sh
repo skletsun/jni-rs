e2edef5d403becea4b961dfb56b0e1db695ae0a8
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-06-13 10:05:51 +0300

    Removed global env vars.

--- .travis/run_travis_job.sh
+++ .travis/run_travis_job.sh
@@ -17,16 +17,17 @@ echo 'Performing checks over the rust code'
 cargo fmt --all -- --check
 
 #JAVA_HOME="${JAVA_HOME:-$(java -XshowSettings:properties -version \
-export JAVA_HOME="$(java -XshowSettings:properties -version \
-    2>&1 > /dev/null |\
-    grep 'java.home' |\
-    awk '{print $3}')"
-LIBJVM_PATH="$(find -L ${JAVA_HOME} -type f -name libjvm.* | xargs -n1 dirname)"
-
-export LD_LIBRARY_PATH="${LIBJVM_PATH}"
 
 # Run clippy static analysis.
 cargo clippy --all --tests --all-features -- -D warnings
+
+#JAVA_HOME="$(java -XshowSettings:properties -version \
+#    2>&1 > /dev/null |\
+#    grep 'java.home' |\
+#    awk '{print $3}')"
+#LIBJVM_PATH="$(find -L ${JAVA_HOME} -type f -name libjvm.* | xargs -n1 dirname)"
+#
+#export LD_LIBRARY_PATH="${LIBJVM_PATH}"
 
 # Run all tests
 cargo test --features=backtrace,invocation
